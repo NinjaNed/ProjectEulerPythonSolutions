b87fe4550b4ef90c71914eb76876669289758a0f
Ned Udomkesmalee <n.udomkesmalee@gmail.com>
2017-08-07 13:38:26 -0700

    problem 4 solution

---
+++ Problem 4.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python 
+__author__ = 'Ned Udomkesmalee'
+
+#Find the largest palindrome made from the product of two 3-digit numbers.
+
+print("Find the largest palindrome made from the product of two 3-digit numbers")
+product = 999 * 999
+while True:
+    if str(product) == str(product)[::-1]:  # if product is palindrome
+        for i in range(100, 999)[::-1]:  # counting backwards from 1000 for efficiency
+            if product % i == 0:
+                factor = product / i # should be an integer
+                if 99 < factor < 1000:
+                    print "Answer is %i" % product
+                    exit()
+
+    product -= 1
+
+
