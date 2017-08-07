3bfb72c93c29e6973a695337bb950c924e917b36
Ned Udomkesmalee <n.udomkesmalee@gmail.com>
2017-08-07 15:25:59 -0700

    problem 6 solution

---
+++ Problem 6.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python 
+__author__ = 'Ned Udomkesmalee'
+
+#Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+
+def problem6():
+    print("Find the difference between the sum of squares and square of sum up to n")
+    while True:
+        n = raw_input("What is n? ")
+        try:
+            n = int(n)
+            if n > 2:
+                square_of_sum = (((n + 1) * n) / 2) ** 2
+                sum_of_square = sum(map(lambda x: x ** 2, range(0, n+1)))
+                print "Answer is %i" % (square_of_sum - sum_of_square)
+                break
+            elif n == 0 or n == 1:
+                print "Answer is 0"
+            else:
+                print "*** Upper bound must be positive ***"
+        except ValueError:
+            print "*** %s is not a valid integer ***" % n
+
+problem6()
