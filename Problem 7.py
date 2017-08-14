b430cb7c4c57e485152c0683b6086f1dbdb9626b
Ned Udomkesmalee <n.udomkesmalee@gmail.com>
2017-08-14 16:35:53 -0700

    problem 7 algorithm improvement

--- Problem 7.py
+++ Problem 7.py
@@ -3,6 +3,7 @@ __author__ = 'Ned Udomkesmalee'
 
 #What is the 10 001st prime number?
 
+#Important facts: Primes > 3 will be of the form 6k +/- 1 and you only need to check for prime divisibility up to sqrt i
 
 def problem7():
     print("Find the nth prime number")
@@ -14,19 +15,23 @@ def problem7():
                 print "*** Upper bound must be a positive non-zero integer ***"
 
             else:
-                prime_list = [2]
-                counter = 1
-                next_num = 3
+                prime_list = [2, 3]
+                counter = 2
+                i = 6
                 while counter < n:
-                    next_num_is_prime = True
-                    for prime in prime_list:
-                        if next_num % prime == 0:
-                            next_num_is_prime = False
-                            break
-                    if next_num_is_prime:
-                        prime_list.append(next_num)
-                        counter += 1
-                    next_num += 2
+                    for next_num in [i-1, i+1]:
+                        next_num_is_prime = True
+                        upper_bound = next_num ** .5
+                        for prime in prime_list:
+                            if next_num % prime == 0:
+                                next_num_is_prime = False
+                                break
+                            elif prime > upper_bound:
+                                break
+                        if next_num_is_prime:
+                            prime_list.append(next_num)
+                            counter += 1
+                    i += 6
                 print "The %ith prime is %i" % (n, prime_list[n-1])
                 break
 
